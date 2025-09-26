bf752c56e4d8b8b25fd34343353484496d66ed29
Harry EMARH <emarh.harry.code@gmail.com>
2025-09-26 11:26:07 +0200

    index 4

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const port = process.env.PORT || 4301;
 
 app.get('/', (req, res) => {
