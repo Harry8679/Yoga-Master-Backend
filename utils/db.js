cdabb65265c19d384bb93fe65a1dd604db1f35e5
Harry EMARH <emarh.harry.code@gmail.com>
2025-09-26 12:44:39 +0200

    db js 2

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log('✅ MongoDB Connected successfully');
+    } catch(err) {
+        console.log('❌ MongoDB connection failed:', err.message);
+    }
+};
+
+module.exports = connectDB;
