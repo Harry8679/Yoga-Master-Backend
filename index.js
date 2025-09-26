484c25210d4cbd4abeea0a486572f4e6ff7f13ee
Harry EMARH <emarh.harry.code@gmail.com>
2025-09-26 13:32:42 +0200

    Connection to database

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 
 const dotenv = require('dotenv');
+const connectDB = require('./utils/db');
 dotenv.config();
 
 const port = process.env.PORT || 4301;
+
+// Connection DB
+connectDB();
+
+// Middleware JSON
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
